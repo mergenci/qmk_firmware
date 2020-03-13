09bfe29f60cdb6d755266e9698e5788edba624be
Cem Mergenci <cmergenci@gmail.com>
2020-03-13 23:39:17 +0300

    Make tapping-and-holding spacebar activate ctrl layer.

--- keyboards/tada68/keymaps/mergenci/keymap.c
+++ keyboards/tada68/keymaps/mergenci/keymap.c
@@ -71,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC,KC_BSLS,  KC_DEL, \
   LT(_CL, KC_ESC),  KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,  KC_ENT, KC_PGUP,  \
   MT(MOD_LSFT, KC_CAPS),   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,   KC_RSFT,        KC_UP,  KC_PGDN, \
-  KC_LCTL, KC_LALT,LM(_AL,0x08),                KC_SPC,                       LM(_AL,0x18),MO(_FL),KC_RCTRL, KC_LEFT,KC_DOWN,KC_RGHT),
+  KC_LCTL, KC_LALT,LM(_AL,0x08),           LT(_CL, KC_SPC),                   LM(_AL,0x18),MO(_FL),KC_RCTRL, KC_LEFT,KC_DOWN,KC_RGHT),
 
   /* Keymap _FL: Function Layer
    * ,----------------------------------------------------------------.
